ef25246d956c15880fd54a50a7b3554ea0fbbc51
TranTam <tamvaa1306@gmail.com>
2024-09-27 14:20:16 +0700

    object types

--- React & TypeScript - The Practical Guide/Section 2: TypeScript Basics & Core Concepts/first-app.ts
+++ React & TypeScript - The Practical Guide/Section 2: TypeScript Basics & Core Concepts/first-app.ts
@@ -1,11 +1,23 @@
 let userName: string = "Max";
-
 // userName = 34
 // Type 'number' is not assignable to type 'string'.
-
 let userAge: number = 30;
 let isValid: boolean = true;
 
 // Union type
 let userID: string | number = "abc";
 userID = 123;
+
+// Object types
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+user = {
+  name: "Max",
+  age: 30,
+  isAdmin: true,
+  id: 123,
+};
