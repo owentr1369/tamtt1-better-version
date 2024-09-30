ced42b62161294f6e9528cda68c13d664c419fbc
TranTam <tamvaa1306@gmail.com>
2024-09-30 17:28:04 +0700

    Setup new project

---
+++ React & TypeScript - The Practical Guide/Section 4: Advanced Component Types - Dynamic Components, Polymorphic Components & More 2/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
+function App() {
+  return <h1>Let's get started</h1>;
+}
+
+export default App;
