c05e2de27803233f9ad0267304af65287c0cfcd7
TranTam <tamvaa1306@gmail.com>
2024-09-30 16:45:37 +0700

    init section 4

---
+++ React & TypeScript - The Practical Guide/Section 4: Advanced Component Types - Dynamic Components, Polymorphic Components & More/src/App.tsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import Header from "./components/Header";
+import goalsImg from "./assets/react.svg";
+import { useState } from "react";
+import CourseGoalList from "./components/CourseGoalList";
+import NewGoal from "./components/NewGoal";
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
+function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  const handleAddGoal = (goal: string, summary: string) => {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        title: goal,
+        description: summary,
+        id: Math.random(),
+      };
+      return [...prevGoals, newGoal];
+    });
+  };
+
+  const handleDeleteGoal = (id: number) => {
+    setGoals((prevGoals) => {
+      return prevGoals.filter((goal) => goal.id !== id);
+    });
+  };
+
+  return (
+    <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <NewGoal onAddGoal={handleAddGoal} />
+      <CourseGoalList goals={goals} handleDeleteGoal={handleDeleteGoal} />
+    </main>
+  );
+}
+
+export default App;
---
+++ React & TypeScript - The Practical Guide/Section 4: Advanced Component Types - Dynamic Components, Polymorphic Components & More/src/components/CourseGoal.tsx
@@ -0,0 +1,29 @@
+import { type PropsWithChildren } from "react";
+
+type CourseGoalProps = PropsWithChildren<{
+  title: string;
+  id: number;
+  handleDeleteGoal: (id: number) => void;
+}>;
+
+const CourseGoal = ({
+  title,
+  id,
+  children,
+  handleDeleteGoal,
+}: CourseGoalProps) => {
+  const onDelete = (id: number) => {
+    handleDeleteGoal(id);
+  };
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <div>{children}</div>
+      </div>
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
