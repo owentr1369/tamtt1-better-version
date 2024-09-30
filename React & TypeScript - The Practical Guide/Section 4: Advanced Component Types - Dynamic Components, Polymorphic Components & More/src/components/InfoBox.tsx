ecdf380c912e238f2218fe81f6861835ffde2f16
TranTam <tamvaa1306@gmail.com>
2024-09-30 17:24:57 +0700

    fixed P tag error

--- React & TypeScript - The Practical Guide/Section 4: Advanced Component Types - Dynamic Components, Polymorphic Components & More/src/components/InfoBox.tsx
+++ React & TypeScript - The Practical Guide/Section 4: Advanced Component Types - Dynamic Components, Polymorphic Components & More/src/components/InfoBox.tsx
@@ -16,18 +16,14 @@ type InfoBoxProps = HintBoxPRops | WarningBoxProps;
 const InfoBox = (props: InfoBoxProps) => {
   const { children, mode } = props;
   if (props.mode === "hint") {
-    return (
-      <aside className="infobox infobox-hint">
-        <p>{children}</p>
-      </aside>
-    );
+    return <aside className="infobox infobox-hint">{children}</aside>;
   }
 
   const { severity } = props;
   return (
     <aside className={`infobox inforbox-warning warning--${severity}`}>
       {mode === "warning" ? <h2>Warning</h2> : null}
-      <p>{children}</p>
+      {children}
     </aside>
   );
 };
