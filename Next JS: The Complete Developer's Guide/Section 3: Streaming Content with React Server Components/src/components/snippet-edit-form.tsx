db2d10be23addc1fc3e5a25a445b10db9d3bbdb3
TranTam <tamvaa1306@gmail.com>
2024-10-01 17:31:11 +0700

    Handling Editor Changes

--- Next JS: The Complete Developer's Guide/Section 3: Streaming Content with React Server Components/src/components/snippet-edit-form.tsx
+++ Next JS: The Complete Developer's Guide/Section 3: Streaming Content with React Server Components/src/components/snippet-edit-form.tsx
@@ -1,12 +1,18 @@
 "use client";
 import type { Snippet } from "@prisma/client";
 import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+
   return (
     <div>
       <Editor
@@ -15,6 +21,7 @@ const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
         language="javascript"
         defaultValue={snippet.code}
         options={{ minimap: { enabled: false } }}
+        onChange={(value) => handleEditorChange(value)}
       />
     </div>
   );
