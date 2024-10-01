307e714751941538a6901d2f77e4293f0421d341
TranTam <tamvaa1306@gmail.com>
2024-10-01 16:18:03 +0700

    Introducing Server Actions in Next.js

--- Next JS: The Complete Developer's Guide/Section 3: Streaming Content with React Server Components/src/app/snippets/new/page.tsx
+++ Next JS: The Complete Developer's Guide/Section 3: Streaming Content with React Server Components/src/app/snippets/new/page.tsx
@@ -1,35 +1,58 @@
-const SnippetCreatePage = () => {
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This needs to be a server action!
+    "use server";
+
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+
+    // Redirect the user back to the root route
+    redirect("/");
+  }
+
   return (
-    <form action="">
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
-          <label htmlFor="title" className="w-12">
+          <label className="w-12" htmlFor="title">
             Title
           </label>
           <input
-            type="text"
-            id="title"
-            className="border rounded p-2 w-full"
             name="title"
+            className="border rounded p-2 w-full"
+            id="title"
           />
         </div>
+
         <div className="flex gap-4">
-          <label htmlFor="code" className="w-12">
+          <label className="w-12" htmlFor="code">
             Code
           </label>
           <textarea
-            id="code"
-            className="border rounded p-2 w-full"
             name="code"
+            className="border rounded p-2 w-full"
+            id="code"
           />
         </div>
+
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
         </button>
       </div>
     </form>
   );
-};
-
-export default SnippetCreatePage;
+}
