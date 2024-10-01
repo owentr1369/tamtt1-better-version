4a0ac8b1fa6252731a3e809c57b8c2fe35c0ce03
TranTam <tamvaa1306@gmail.com>
2024-10-01 17:08:00 +0700

    A Few Quick Tasks

--- Next JS: The Complete Developer's Guide/Section 3: Streaming Content with React Server Components/src/app/page.tsx
+++ Next JS: The Complete Developer's Guide/Section 3: Streaming Content with React Server Components/src/app/page.tsx
@@ -1,14 +1,31 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
   const renderedSnippets = snippets.map((snippet) => {
     return (
-      <div key={snippet.id}>
-        <h3>{snippet.title}</h3>
-        <pre>{snippet.code}</pre>
-      </div>
+      <Link
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+        key={snippet.id}
+      >
+        <div>
+          <h3>{snippet.title}</h3>
+        </div>
+        <div>View</div>
+      </Link>
     );
   });
-  return <h1>{renderedSnippets}</h1>;
+  return (
+    <div>
+      <div className="flex my-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex gap-2 flex-col">{renderedSnippets}</div>
+    </div>
+  );
 }
