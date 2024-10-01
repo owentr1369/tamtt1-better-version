36083f053b986948aaf2c39c5a4a1c5054989aa1
TranTam <tamvaa1306@gmail.com>
2024-10-01 16:48:04 +0700

    Fetching Particular Records

--- Next JS: The Complete Developer's Guide/Section 3: Streaming Content with React Server Components/src/app/snippets/[id]/page.tsx
+++ Next JS: The Complete Developer's Guide/Section 3: Streaming Content with React Server Components/src/app/snippets/[id]/page.tsx
@@ -1,6 +1,22 @@
-const SnippetShowPage = (props: any) => {
-  console.log(props);
-  return <div>SnippetShowPage</div>;
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const SnippetShowPage = async (props: SnippetShowPageProps) => {
+  const snippet = await db.snippet.findUnique({
+    where: {
+      id: Number(props.params.id),
+    },
+  });
+  if (!snippet) {
+    return notFound();
+  }
+  return <div>{snippet.title}</div>;
 };
 
 export default SnippetShowPage;
