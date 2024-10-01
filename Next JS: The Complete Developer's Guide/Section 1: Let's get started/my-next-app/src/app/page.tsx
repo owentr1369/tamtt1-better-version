681637220989ad629c525bf16b335e9dc17488f7
TranTam <tamvaa1306@gmail.com>
2024-10-01 13:51:44 +0700

    Common UI in Next JS with Layouts

--- Next JS: The Complete Developer's Guide/Section 1: Let's get started/my-next-app/src/app/page.tsx
+++ Next JS: The Complete Developer's Guide/Section 1: Let's get started/my-next-app/src/app/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
       <h2>This is home page</h2>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
     </div>
   );
 }
