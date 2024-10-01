c921942e14b2558c5ba0a59e8fbada4688a6d0f8
TranTam <tamvaa1306@gmail.com>
2024-10-01 13:46:33 +0700

    Linking between pages

--- Next JS: The Complete Developer's Guide/Section 1: Let's get started/my-next-app/src/app/page.tsx
+++ Next JS: The Complete Developer's Guide/Section 1: Let's get started/my-next-app/src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <h2>This is home page</h2>
+      <div>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+      </div>
+    </div>
+  );
 }
