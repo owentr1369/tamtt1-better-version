5dfe3e29dc8eb116c6561adf5fd82b92642e9c88
TranTam <tamvaa1306@gmail.com>
2024-10-02 11:01:55 +0700

    Gotchas Around Error Handling in Nextjs

--- Next JS: The Complete Developer's Guide/Section 5: Server Forms with the UseFormState Hook/src/actions/index.ts
+++ Next JS: The Complete Developer's Guide/Section 5: Server Forms with the UseFormState Hook/src/actions/index.ts
@@ -28,23 +28,32 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  // Check the user's inputs and make sure they're valid
-  const title = formData.get("title");
-  const code = formData.get("code");
+  try {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-  if (typeof title !== "string" || title.length < 3) {
-    return { message: "Title must be longer" };
-  } else if (typeof code !== "string" || code.length < 3) {
-    return { message: "Code must be longer" };
+    if (typeof title !== "string" || title.length < 3) {
+      return { message: "Title must be longer" };
+    } else if (typeof code !== "string" || code.length < 3) {
+      return { message: "Code must be longer" };
+    }
+
+    // Create a new record in the database
+    // const snippet = await db.snippet.create({
+    //   data: {
+    //     title,
+    //     code,
+    //   },
+    // });
+    throw new Error("Failed to save to database");
+    // Redirect the user back to the root route
+    redirect("/");
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    } else {
+      return { message: "Something went wrong" };
+    }
   }
-
-  // Create a new record in the database
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
-  // Redirect the user back to the root route
-  redirect("/");
 }
