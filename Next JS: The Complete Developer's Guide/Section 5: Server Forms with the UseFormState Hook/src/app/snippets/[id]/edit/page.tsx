092f13683d5d865df30c5dca14edfe2325120942
TranTam <tamvaa1306@gmail.com>
2024-10-02 10:28:24 +0700

    init section 5

---
+++ Next JS: The Complete Developer's Guide/Section 5: Server Forms with the UseFormState Hook/src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,31 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import SnippetEditForm from "@/components/snippet-edit-form";
+import * as actions from "@/actions";
+
+interface SnippetEditPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const SnippetEditPage = async (props: SnippetEditPageProps) => {
+  const id = parseInt(props.params.id);
+
+  const snippet = await db.snippet.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
+};
+
+export default SnippetEditPage;
---
+++ Next JS: The Complete Developer's Guide/Section 5: Server Forms with the UseFormState Hook/src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+const SnippetNotFoundPage = () => {
+  return (
+    <div className="text-xl bold">
+      Sorry, but we couldn't find the snippet you were looking for.
+    </div>
+  );
+};
+
+export default SnippetNotFoundPage;
