5957f008ef38f162e514639dd6f7a5d0391be960
TranTam <tamvaa1306@gmail.com>
2024-10-03 17:50:37 +0700

    handling user inputs

---
+++ A complete, all-in-one guide to fully testing your React projects using React Testing Library and Jest/Section 2: A Whirlwind Overview of Testing/my-app/src/components/UserForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./userForm.css";
+
+const UserForm = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data Submitted: ", {
+      name,
+      email,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="user-form">
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={handleChangeName}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleChangeEmail}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default UserForm;
